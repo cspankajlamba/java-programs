ed4f9557400781dadeb940292e9c10deb2329614
Coders Corner <112269355+coderscorner-cc@users.noreply.github.com>
2023-02-06 11:12:38 +0530

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,8 @@
+public class typecasting {
+    public static void main(String[] args){
+        int num = 9;
+        System.out.println(num);
+        double fnum = (double) num; //manual type casting - Integar to double data type
+        System.out.println(fnum);
+    }
+}
